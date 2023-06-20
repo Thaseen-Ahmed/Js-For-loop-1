eba2547e239f7c123ef40c5f77c5d5401117de8a
ThaseenPS <thaseenahmedps235@gmail.com>
2023-06-20 11:54:54 +0530

    change3

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ document.write("<br>")
 document.write("<br>")
 // document.write("<br>")
 document.write("Sum Of The Given Array is " +sum)
+
+document.write("<br>")
+document.write("<br>")
+
+
+
+let z= document.getElementById("hii");
+z.innerHTML="<h2>Thii is Solution for othaner Problem</h2>";
+
+
+
+
+
+
+
+document.write("<h1>This Is Solution for Another Problem</h1>")
+
+let str=prompt("Enter a String you Want")
+
+// for(i=0;i<str.length;i++){
+//     document.write(i)
+// }
+
+for(let b of str){
+    document.write(b);
+    document.write("<br>");
+    // console.log(b)
+}
