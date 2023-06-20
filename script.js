5223f47aac20c494726256377e4374c82ffa7449
ThaseenPS <thaseenahmedps235@gmail.com>
2023-06-20 11:11:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.write("The Given Array is [1,2,3,4,5]");
+
+let arr=[1,2,3,4,5];
+
+let sum=0;
+for(i=0;i<arr.length;i++){
+    sum += arr[i];
+}
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("Sum Of The Given Array is " +sum)
